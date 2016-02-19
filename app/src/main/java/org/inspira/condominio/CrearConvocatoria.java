a02184edcbf2bed530bcb803c1344e2a308a345e
chesco134 <chesco134@gmail.com>
2016-02-19 12:34:07 -0600

    Primeros pasos para generar una convocatoria. (Plantilla)

---
+++ app/src/main/java/org/inspira/condominio/CrearConvocatoria.java
@@ -0,0 +1,16 @@
+package org.inspira.condominio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jcapiz on 19/02/16.
+ */
+public class CrearConvocatoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hacer_convocatoria);
+    }
+}
